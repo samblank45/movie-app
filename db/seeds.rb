09c735a4b41e1906f155d811f68f5168d6a99041
sam <sblan45@gmail.com>
2020-05-28 02:15:14 -0400

    adds FFaker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-actor = Actor.new({first_name: "Bruce", last_name: "Willis", known_for: "Die hard"})
-actor.save
+# actor = Actor.new({first_name: "Bruce", last_name: "Willis", known_for: "Die hard"})
+# actor.save
+
+# FFaker::Name.unique.name 
