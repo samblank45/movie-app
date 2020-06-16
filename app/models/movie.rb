e2568ce7447005a6cd594d7341d53e0d1ead885f
sam <sblan45@gmail.com>
2020-06-15 20:35:55 -0400

    adds associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,11 @@ class Movie < ApplicationRecord
   validates :plot, length: {maximum: 500}
 
   has_many :actors
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map {|genre| genre.name}
+  end
 end
